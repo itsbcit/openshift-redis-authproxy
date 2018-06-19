7a9527673a4e44f994555881d6f1b84ff2508d27
Jesse Weisner <jesse_weisner@bcit.ca>
2018-06-19 10:59:35 -0700

    bash substring not working

--- 70-stunnel-psk.sh
+++ 70-stunnel-psk.sh
@@ -1,6 +1,6 @@
 export STUNNEL_PSK_FILE="${STUNNEL_PSK_FILE:-/etc/stunnel/psk.txt}"
 export STUNNEL_VERIFY_CHAIN="${STUNNEL_VERIFY_CHAIN:-yes}"
-export STUNNEL_CHECKHOST="${STUNNEL_CONNECT%:*}"
+export STUNNEL_CHECKHOST=$(echo $STUNNEL_CONNECT|cut -f1 -d ':')
 
 if [[ -z "${STUNNEL_CONNECT}" ]] || [[ -z "${STUNNEL_PSK}" ]];then
     echo >&2 "missing STUNNEL_CONNECT and/or STUNNEL_PSK"
