f837bae998364f233fca920bd32ac86e0bb15749
Jesse Weisner <jesse_weisner@bcit.ca>
2019-09-13 11:03:19 -0700

    as found

--- 70-stunnel-psk.sh
+++ 70-stunnel-psk.sh
@@ -1,13 +1,8 @@
-export STUNNEL_PSK_FILE="${STUNNEL_PSK_FILE:-/etc/stunnel/psk.txt}"
-export STUNNEL_VERIFY_CHAIN="${STUNNEL_VERIFY_CHAIN:-yes}"
-export STUNNEL_CHECKHOST=$(echo $STUNNEL_CONNECT|cut -f1 -d ':')
-
 if [[ -z "${STUNNEL_CONNECT}" ]] || [[ -z "${STUNNEL_PSK}" ]];then
     echo >&2 "missing STUNNEL_CONNECT and/or STUNNEL_PSK"
     exit 1
 fi
 
 cat ${STUNNEL_CONF_TEMPLATE} | envsubst > ${STUNNEL_CONF}
-
 
 echo ${STUNNEL_PSK} > ${STUNNEL_PSK_FILE}
--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ COPY stunnel.conf.template /etc/stunnel/stunnel.conf.template
 COPY 70-stunnel-psk.sh /docker-entrypoint.d/
 
 ENV STUNNEL_ACCEPT 0.0.0.0:6379
+ENV STUNNEL_DEBUG 5
 ENV STUNNEL_SERVICE redis
 ENV STUNNEL_CONNECT ""
 ENV STUNNEL_PSK ""
-ENV STUNNEL_CACERT /etc/ssl/certs/ca-certificates.crt
-ENV STUNNEL_VERIFY_CHAIN yes
+ENV STUNNEL_PSK_FILE /etc/stunnel/psk.txt
 EXPOSE 6379
